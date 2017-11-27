34f3362785ba10925029bef2ca40de271fc36de6
Mark Simms <masimms@microsoft.com>
2017-11-27 13:56:37 -0800

    Removing ai exploration from mainline

--- AiTest/AiTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AiTest.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- AiTest/AiTest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AiTest.WebApiApplication" Language="C#" %>
