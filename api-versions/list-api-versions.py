3e7a1fd4621bb605557490ddac0f6a9ee88f8f50
Mark Simms <masimms@microsoft.com>
2018-09-24 11:59:08 -0700

    Updating simpler approach

--- api-versions/list-api-versions.py
+++ api-versions/list-api-versions.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python
 
-import os
 import json
-from azure.common.credentials import ServicePrincipalCredentials
-from azure.mgmt.resource import ResourceManagementClient
+import datetime
 
-subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID')
-credentials = ServicePrincipalCredentials(
-    client_id=os.environ['AZURE_CLIENT_ID'],
-    secret=os.environ['AZURE_CLIENT_SECRET'],
-    tenant=os.environ['AZURE_TENANT_ID']
-)
-resource_client = ResourceManagementClient(credentials, subscription_id)
+from collections import namedtuple
 
+ProviderRegistration = namedtuple('ProviderRegistration', ['namespace', 'resourceType', 'apiVersion'])
+
+with open("providers.json", "r") as read_file:
+    data = json.load(read_file)
+
+providers=[]
+
+for provider in data:
+    namespace = provider["namespace"]
+    resourceTypes = provider["resourceTypes"]
+    print namespace
+
+    for resource in resourceTypes:
+        resourceType = resource["resourceType"]
+
+#        for api in resource["apiVersions"]:
+            #print "{0},{1},{2}".format(namespace, resourceType, api)
+        if len(resource["apiVersions"]) > 0:            
+            apiVersions=sorted(resource["apiVersions"], reverse=True)
+            latestApi=max(resource["apiVersions"])                    
+            prov = ProviderRegistration(namespace, resourceType, latestApi)        
+            providers.append(prov)
+
+today = datetime.datetime.now()
+
+for provider in providers:    
+    if provider.namespace.startswith('Microsoft'):
+        if provider.apiVersion.endswith('-preview'):
+            previewDate = datetime.datetime.strptime(provider.apiVersion[:10], '%Y-%m-%d')
+            delta = today - previewDate
+            print "{0},{1},{2},{3}".format(provider.namespace, provider.resourceType, provider.apiVersion, delta.days)            
+        else:
+            print "{0},{1},{2},{3}".format(provider.namespace, provider.resourceType, provider.apiVersion, "")            
